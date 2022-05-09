fbd78537c7ac13602462e7cff7219ce8dcbb205e
theEmperorofDaiViet <Khiet.To.05012001@gmail.com>
2022-05-09 22:04:50 +0700

    first commit

---
+++ src/test/java/jersey/NewJerseyBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package jersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJerseyBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
